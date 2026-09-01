8bd614e1c0e6bab58ccc5dbc3e49628eeb86c91f
James Robert Huggins Ngo <jamesroberthugginsngo@gmail.com>
2026-08-31 22:15:15 -0400

    remove sandbox

--- src/sandbox.js
+++
@@ -1,9 +0,0 @@
-const proxy = new Proxy([], {
-	set: (target, property, value) => {
-		const oldValue = target[property];
-		console.log({ target, property, value, oldValue });
-		return Reflect.set(target, property, value);
-	}
-});
-
-proxy.push('Alice');
