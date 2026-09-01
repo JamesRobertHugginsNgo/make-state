54cb8bab5ec7fe909f5aa290cc052e1aed06ab33
James Robert Huggins Ngo <jamesroberthugginsngo@gmail.com>
2026-09-01 18:54:02 -0400

    add sha information

--- src/tracked-event-target.js
+++ src/tracked-event-target.js
@@ -1,5 +1,9 @@
 // source: https://github.com/JamesRobertHugginsNgo/tracked-event-target/blob/2.0.0/src/tracked-event-target.js
 
+// ls-remote repository: https://github.com/JamesRobertHugginsNgo/tracked-event-target.git
+// ls-remote branch: refs/heads/2x
+// ls-remote branch sha: e67864e143581ac87cb808ee5df8d2c6657291c0
+
 import ListenerTracker from './event-listener-tracker.js';
 
 export default class TrackedEventTarget extends EventTarget {
