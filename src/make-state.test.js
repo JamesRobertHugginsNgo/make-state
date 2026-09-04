ee25399f4aadc21bbc1466a8d48aa2738e2fa09c
James Robert Huggins Ngo <jamesroberthugginsngo@gmail.com>
2026-09-04 15:24:11 -0400

    refactor and add tests

--- src/make-state.test.js
+++ src/make-state.test.js
@@ -14,8 +14,7 @@ describe('make-state.test.js', async () => {
 	test('make undefined state', () => {
 		const [proxy, eventTarget] = makeState();
 
-		assert.strictEqual(typeof proxy, 'object');
-		assert.ok(!Array.isArray(proxy));
+		assert.deepStrictEqual(proxy, {});
 		assert.ok(eventTarget instanceof TrackedEventTarget);
 		assert.ok(eventTargetRegistry.has(proxy));
 		assert.strictEqual(eventTargetRegistry.get(proxy), eventTarget);
@@ -26,9 +25,7 @@ describe('make-state.test.js', async () => {
 			const target = { name: 'Alice' };
 			const [proxy] = makeState(target);
 
-			assert.strictEqual(typeof proxy, 'object');
-			assert.ok(!Array.isArray(proxy));
-			assert.deepEqual(target, proxy);
+			assert.deepStrictEqual(proxy, { name: 'Alice' });
 		});
 
 		describe('change event', async () => {
@@ -42,16 +39,15 @@ describe('make-state.test.js', async () => {
 
 				assert.ok(result instanceof CustomEvent);
 				assert.ok('change' in result.detail);
-				assert.strictEqual(result.detail.change.target, target);
-				assert.strictEqual(result.detail.change.property, 'name');
-				assert.strictEqual(result.detail.change.oldValue, 'Alice');
-				assert.strictEqual(result.detail.change.proxy, proxy);
-				assert.ok(Array.isArray(result.detail.change.path));
-				assert.strictEqual(result.detail.change.path[0], 'name');
-				assert.ok(Array.isArray(result.detail.change.keys));
-				assert.strictEqual(result.detail.change.keys[0], 'name');
-				assert.ok(result.detail.change.dispatched instanceof Set);
-				assert.ok(result.detail.change.dispatched.has(eventTarget));
+				assert.deepStrictEqual(result.detail.change, {
+					target: { name: 'Bob' },
+					property: 'name',
+					oldValue: 'Alice',
+					proxy,
+					path: ['name'],
+					keys: ['name'],
+					dispatched: new Set([eventTarget])
+				});
 			});
 
 			test('change object property as insertion', () => {
@@ -60,10 +56,19 @@ describe('make-state.test.js', async () => {
 
 				let result;
 				eventTarget.addEventListener(CHANGE_EVENT_TYPE, (event) => { result = event; }, { once: true });
-				proxy.name = 'Bob';
+				proxy.name = 'Alice';
 
-				assert.strictEqual(result.detail.change.property, 'name');
-				assert.strictEqual(result.detail.change.oldValue, undefined);
+				assert.ok(result instanceof CustomEvent);
+				assert.ok('change' in result.detail);
+				assert.deepStrictEqual(result.detail.change, {
+					target: { name: 'Alice' },
+					property: 'name',
+					oldValue: undefined,
+					proxy,
+					path: ['name'],
+					keys: ['name'],
+					dispatched: new Set([eventTarget])
+				});
 			});
 
 			test('change object property as removal', () => {
@@ -74,8 +79,17 @@ describe('make-state.test.js', async () => {
 				eventTarget.addEventListener(CHANGE_EVENT_TYPE, (event) => { result = event; }, { once: true });
 				delete proxy.name;
 
-				assert.strictEqual(result.detail.change.property, 'name');
-				assert.strictEqual(result.detail.change.oldValue, 'Alice');
+				assert.ok(result instanceof CustomEvent);
+				assert.ok('change' in result.detail);
+				assert.deepStrictEqual(result.detail.change, {
+					target: {},
+					property: 'name',
+					oldValue: 'Alice',
+					proxy,
+					path: ['name'],
+					keys: ['name'],
+					dispatched: new Set([eventTarget])
+				});
 			});
 
 			test('change object property twice', () => {
@@ -89,11 +103,26 @@ describe('make-state.test.js', async () => {
 				proxy.name = 'Bob';
 				eventTarget.removeEventListener(CHANGE_EVENT_TYPE, listener);
 
-				assert.strictEqual(results.length, 2);
-				assert.strictEqual(results[0].detail.change.property, 'name');
-				assert.strictEqual(results[0].detail.change.oldValue, undefined);
-				assert.strictEqual(results[1].detail.change.property, 'name');
-				assert.strictEqual(results[1].detail.change.oldValue, 'Alice');
+				const expectedBase = {
+					target: { name: 'Bob' },
+					property: 'name',
+					proxy,
+					path: ['name'],
+					keys: ['name'],
+					dispatched: new Set([eventTarget])
+				};
+				assert.ok(results[0] instanceof CustomEvent);
+				assert.ok('change' in results[0].detail);
+				assert.deepStrictEqual(results[0].detail.change, {
+					...expectedBase,
+					oldValue: undefined
+				});
+				assert.ok(results[1] instanceof CustomEvent);
+				assert.ok('change' in results[1].detail);
+				assert.deepStrictEqual(results[1].detail.change, {
+					...expectedBase,
+					oldValue: 'Alice'
+				});
 			});
 		});
 
@@ -107,13 +136,65 @@ describe('make-state.test.js', async () => {
 				proxy.name = 'Bob';
 				await Promise.resolve();
 
+				const expectedRaw = [{
+					target: { name: 'Bob' },
+					property: 'name',
+					oldValue: 'Alice',
+					proxy,
+					path: ['name'],
+					keys: ['name'],
+					dispatched: new Set([eventTarget])
+				}];
 				assert.ok(result instanceof CustomEvent);
 				assert.ok('raw' in result.detail);
-				assert.ok(Array.isArray(result.detail.raw));
-				assert.strictEqual(result.detail.raw.length, 1);
+				assert.deepStrictEqual(result.detail.raw, expectedRaw);
 				assert.ok('changes' in result.detail);
-				assert.ok(result.detail.changes instanceof Map);
-				assert.ok(result.detail.changes.has('name'));
+				assert.deepStrictEqual(result.detail.changes, new Map([['name', expectedRaw[0]]]));
+			});
+
+			test('change object properties multiple', async () => {
+				const target = { name: 'Alice' };
+				const [proxy, eventTarget] = makeState(target);
+
+				let result;
+				eventTarget.addEventListener(BATCH_CHANGE_EVENT_TYPE, (event) => { result = event; }, { once: true });
+				proxy.name = 'Bob';
+				proxy.name = 'Charlie';
+				proxy.age = 30;
+				await Promise.resolve();
+
+				const expectedBase = {
+					target: { name: 'Charlie', age: 30 },
+					proxy,
+					dispatched: new Set([eventTarget])
+				};
+				const expectedRaw = [{
+					...expectedBase,
+					property: 'name',
+					oldValue: 'Alice',
+					path: ['name'],
+					keys: ['name']
+				}, {
+					...expectedBase,
+					property: 'name',
+					oldValue: 'Bob',
+					path: ['name'],
+					keys: ['name']
+				}, {
+					...expectedBase,
+					property: 'age',
+					oldValue: undefined,
+					path: ['age'],
+					keys: ['age']
+				}];
+				assert.ok(result instanceof CustomEvent);
+				assert.ok('raw' in result.detail);
+				assert.deepStrictEqual(result.detail.raw, expectedRaw);
+				assert.ok('changes' in result.detail);
+				assert.deepStrictEqual(result.detail.changes, new Map([
+					['name', expectedRaw[0]],
+					['age', expectedRaw[2]]
+				]));
 			});
 		});
 	});
@@ -123,9 +204,7 @@ describe('make-state.test.js', async () => {
 			const target = ['Alice'];
 			const [proxy] = makeState(target);
 
-			assert.strictEqual(typeof proxy, 'object');
-			assert.ok(Array.isArray(proxy));
-			assert.deepEqual(target, proxy);
+			assert.deepStrictEqual(proxy, ['Alice']);
 		});
 
 		describe('change event', () => {
@@ -139,12 +218,15 @@ describe('make-state.test.js', async () => {
 
 				assert.ok(result instanceof CustomEvent);
 				assert.ok('change' in result.detail);
-				assert.strictEqual(result.detail.change.target, target);
-				assert.strictEqual(result.detail.change.property, '0');
-				assert.strictEqual(result.detail.change.oldValue, 'Alice');
-				assert.strictEqual(result.detail.change.proxy, proxy);
-				assert.ok(result.detail.change.dispatched instanceof Set);
-				assert.ok(result.detail.change.dispatched.has(eventTarget));
+				assert.deepStrictEqual(result.detail.change, {
+					target: ['Bob'],
+					property: '0',
+					oldValue: 'Alice',
+					proxy,
+					path: ['0'],
+					keys: ['0'],
+					dispatched: new Set([eventTarget])
+				});
 			});
 
 			test('change array element as insertion', () => {
@@ -155,8 +237,17 @@ describe('make-state.test.js', async () => {
 				eventTarget.addEventListener(CHANGE_EVENT_TYPE, (event) => { result = event; });
 				proxy.push('Alice');
 
-				assert.strictEqual(result.detail.change.property, '0');
-				assert.strictEqual(result.detail.change.oldValue, undefined);
+				assert.ok(result instanceof CustomEvent);
+				assert.ok('change' in result.detail);
+				assert.deepStrictEqual(result.detail.change, {
+					target: ['Alice'],
+					property: '0',
+					oldValue: undefined,
+					proxy,
+					path: ['0'],
+					keys: ['0'],
+					dispatched: new Set([eventTarget])
+				});
 			});
 
 			test('change array element as removal', () => {
@@ -168,8 +259,17 @@ describe('make-state.test.js', async () => {
 				let element = proxy.pop();
 
 				assert.strictEqual(element, 'Alice');
-				assert.strictEqual(result.detail.change.property, '0');
-				assert.strictEqual(result.detail.change.oldValue, 'Alice');
+				assert.ok(result instanceof CustomEvent);
+				assert.ok('change' in result.detail);
+				assert.deepStrictEqual(result.detail.change, {
+					target: [],
+					property: '0',
+					oldValue: 'Alice',
+					proxy,
+					path: ['0'],
+					keys: ['0'],
+					dispatched: new Set([eventTarget])
+				});
 			});
 
 			test('change array element twice', () => {
@@ -178,20 +278,55 @@ describe('make-state.test.js', async () => {
 
 				let results = [];
 				const listener = (event) => { results.push(event); };
+
 				eventTarget.addEventListener(CHANGE_EVENT_TYPE, listener);
 				proxy.push('Alice');
-				proxy.pop();
+				const element = proxy.pop();
 				proxy.push('Bob');
 				eventTarget.removeEventListener(CHANGE_EVENT_TYPE, listener);
 
-				assert.strictEqual(results.length, 4);
-				assert.strictEqual(results[0].detail.change.property, '0');
-				assert.strictEqual(results[0].detail.change.oldValue, undefined);
-				assert.strictEqual(results[1].detail.change.property, '0');
-				assert.strictEqual(results[1].detail.change.oldValue, 'Alice');
-				assert.strictEqual(results[2].detail.change.property, 'length'); // array set trap quirk - change visible on pop but not push
-				assert.strictEqual(results[0].detail.change.property, '0');
-				assert.strictEqual(results[0].detail.change.oldValue, undefined);
+				const expectedBase = {
+					target: ['Bob'],
+					proxy,
+					dispatched: new Set([eventTarget])
+				};
+				assert.strictEqual(element, 'Alice');
+				assert.ok(results[0] instanceof CustomEvent);
+				assert.ok('change' in results[0].detail);
+				assert.deepStrictEqual(results[0].detail.change, {
+					...expectedBase,
+					property: '0',
+					oldValue: undefined,
+					path: ['0'],
+					keys: ['0']
+				});
+				assert.ok(results[1] instanceof CustomEvent);
+				assert.ok('change' in results[1].detail);
+				assert.deepStrictEqual(results[1].detail.change, {
+					...expectedBase,
+					property: '0',
+					oldValue: 'Alice',
+					path: ['0'],
+					keys: ['0']
+				});
+				assert.ok(results[2] instanceof CustomEvent);
+				assert.ok('change' in results[2].detail);
+				assert.deepStrictEqual(results[2].detail.change, {
+					...expectedBase,
+					property: 'length',
+					oldValue: 1,
+					path: ['length'],
+					keys: ['length']
+				}); // javascript quirk - array pop makes length change visible for pop method
+				assert.ok(results[3] instanceof CustomEvent);
+				assert.ok('change' in results[3].detail);
+				assert.deepStrictEqual(results[3].detail.change, {
+					...expectedBase,
+					property: '0',
+					oldValue: undefined,
+					path: ['0'],
+					keys: ['0']
+				});
 			});
 		});
 
@@ -202,17 +337,23 @@ describe('make-state.test.js', async () => {
 
 				let result;
 				eventTarget.addEventListener(BATCH_CHANGE_EVENT_TYPE, (event) => { result = event; }, { once: true });
-				proxy.pop();
-				proxy.push('Bob');
+				proxy[0] = 'Bob';
 				await Promise.resolve();
 
+				const expected = {
+					target: ['Bob'],
+					property: '0',
+					oldValue: 'Alice',
+					proxy,
+					path: ['0'],
+					keys: ['0'],
+					dispatched: new Set([eventTarget])
+				};
 				assert.ok(result instanceof CustomEvent);
 				assert.ok('raw' in result.detail);
-				assert.ok(Array.isArray(result.detail.raw));
-				assert.strictEqual(result.detail.raw.length, 3);
+				assert.deepStrictEqual(result.detail.raw, [expected]);
 				assert.ok('changes' in result.detail);
-				assert.ok(result.detail.changes instanceof Map);
-				assert.ok(result.detail.changes.has('0'));
+				assert.deepStrictEqual(result.detail.changes, new Map([['0', expected]]));
 			});
 		});
 	});
@@ -226,11 +367,11 @@ describe('make-state.test.js', async () => {
 
 			assert.strictEqual(typeof addressProxy, 'object');
 			assert.ok(!Array.isArray(addressProxy));
-			assert.deepEqual(addressTarget, addressProxy);
+			assert.deepStrictEqual(addressTarget, addressProxy);
 			assert.ok(eventTargetRegistry.has(addressProxy));
 			assert.strictEqual(typeof personProxy, 'object');
 			assert.ok(!Array.isArray(personProxy));
-			assert.deepEqual(personTarget, personProxy);
+			assert.deepStrictEqual(personTarget, personProxy);
 			assert.ok(eventTargetRegistry.has(personProxy));
 		});
 
@@ -256,6 +397,7 @@ describe('make-state.test.js', async () => {
 				assert.ok(result.detail.change.dispatched instanceof Set);
 				assert.ok(result.detail.change.dispatched.has(eventTarget));
 			});
+
 		});
 	});
 });
